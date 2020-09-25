5c9822f615c965a2e2193ab34b8bf5964f59770e
amandamata <amanda.mata@live.com>
2020-09-25 20:26:10 -0300

    task and user models

---
+++ MyTasksAPI/MyTasksAPI/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyTasksAPI.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime HourDate { get; set; }
+        public DateTime Updated { get; set; }
+        public DateTime Created { get; set; }
+        public string Place { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
---
+++ MyTasksAPI/MyTasksAPI/Models/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyTasksAPI.Models
+{
+    public class UserDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string PersistPassword { get; set; }
+    }
+}
