0a6c8193e178e94e2f4d62907ca7e82c0a9bdecd
amandamata <amanda.mata@live.com>
2020-09-26 13:19:01 -0300

    user repository

---
+++ MyTasksAPI/MyTasksAPI/Repositories/Contracts/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using MyTasksAPI.Models;
+
+namespace MyTasksAPI.Repositories.Contracts
+{
+    public interface IUserRepository
+    {
+        void Register(ApplicationUser user, string password);
+        ApplicationUser Get(string email, string password);
+    }
+}
---
+++ MyTasksAPI/MyTasksAPI/Repositories/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MyTasksAPI.Models;
+using MyTasksAPI.Repositories.Contracts;
+using System;
+using System.Text;
+
+namespace MyTasksAPI.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UserRepository(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        
+        public ApplicationUser Get(string email, string password)
+        {
+            var user = _userManager.FindByEmailAsync(email).Result;
+            if (user != null)
+                if (_userManager.CheckPasswordAsync(user, password).Result) return user;
+            return new ApplicationUser();
+        }
+        
+        public void Register(ApplicationUser user, string password)
+        {
+            var result = _userManager.CreateAsync(user, password).Result;
+            if (!result.Succeeded)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach(var error in result.Errors)
+                {
+                    sb.Append(error.Description);
+                }
+                throw new Exception($"User not registered! {sb.ToString()}");
+            }
+        }
+    }
+}
