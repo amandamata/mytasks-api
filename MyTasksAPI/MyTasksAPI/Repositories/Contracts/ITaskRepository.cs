2a7d3c7f3a9d1ebc6c66de8a5088bb5e010efa91
amandamata <amanda.mata@live.com>
2020-09-26 13:44:59 -0300

    task repository

---
+++ MyTasksAPI/MyTasksAPI/Repositories/Contracts/ITaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyTasksAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MyTasksAPI.Repositories.Contracts
+{
+    public interface ITaskRepository
+    {
+        List<Task> Sinc(List<Task> tasks);
+        List<Task> Restore(ApplicationUser user, DateTime lastSincDate);
+    }
+}
---
+++ MyTasksAPI/MyTasksAPI/Repositories/TaskRepository.cs
@@ -0,0 +1,33 @@
+﻿using MyTasksAPI.Database;
+using MyTasksAPI.Models;
+using MyTasksAPI.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyTasksAPI.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly MyTasksContext _database;
+        public TaskRepository(MyTasksContext database)
+        {
+            database = _database
+        }
+
+        public List<Task> Restore(ApplicationUser user, DateTime lastSincDate)
+        {
+            var query = _database.Tasks.Where(a => a.IdUser == user.Id).AsQueryable();
+            if (lastSincDate != null)
+            {
+                query.Where(a => a.Created >= lastSincDate || a.Updated >= lastSincDate);
+            }
+            return query.ToList();
+        }
+
+        public List<Task> Sinc(List<Task> tasks)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
